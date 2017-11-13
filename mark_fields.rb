4bafb1f17dc82ebb44dbd1b608256bf36efd7641
Charles Duan <cduan@sbf5.com>
2017-11-13 00:25:33 -0500

    Added rectangle detection

--- mark_fields.rb
+++ mark_fields.rb
@@ -73,6 +73,156 @@ class PdfFileParser
 
 end
 
+class MarkingUI
+
+  def initialize(parser)
+
+    @parser = parser
+    @resolution = 144
+
+    @root = TkRoot.new {
+      title("Mark Form Fields")
+    }
+
+    initialize_top_frame
+
+    initialize_canvas_frame
+
+    load_image(1)
+
+  end
+
+  def initialize_top_frame
+    frame = TkFrame.new(@root)
+    frame.pack(fill: 'x', expand: true, side: 'top')
+
+    @prev_page_button = TkButton.new(
+      frame, text: "< Previous Page",
+      command: proc { load_image(@page - 1) }
+    )
+    @prev_page_button.pack(side: 'left')
+
+    @next_page_button = TkButton.new(
+      frame, text: "Next Page >",
+      command: proc { load_image(@page + 1) }
+    )
+    @next_page_button.pack(side: 'left')
+
+  end
+
+  def initialize_canvas_frame
+
+    frame = TkFrame.new(@root)
+    frame.pack(fill: 'x', expand: true, side: 'top')
+
+    @canvas = TkCanvas.new(
+      frame,
+      width: @resolution * 17 /2,
+      height: 500,
+      scrollregion: "0 0 #{@resolution * 17 / 2} #{@resolution * 11}"
+    )
+    @canvas.pack(fill: 'both', expand: 1, side: 'left')
+
+    @canvas.itembind('image', '1') do |e|
+      process_click(e)
+    end
+    @canvas.itembind('rect', '1') do |e|
+      @canvas.find_withtag('current')[0].destroy
+    end
+
+    @vscroll = TkScrollbar.new(frame) { orient 'vertical' }
+    @canvas.yscrollbar(@vscroll)
+    @vscroll.pack(side: 'left', fill: 'y', expand: 1)
+
+    frame.bind_all("MouseWheel") do |e|
+      @canvas.yview_scroll(-e.delta, "units")
+    end
+  end
+
+  def process_click(e)
+    cx, cy = @canvas.canvasx(e.x).round, @canvas.canvasy(e.y).round
+    puts "Got #{cx}, #{cy}"
+    coords = find_box(cx, cy)
+    if coords
+      TkcRectangle.new(@canvas, *coords, fill: 'red', width: 0, tags: 'rect')
+    end
+  end
+
+  def find_box(x, y)
+    start_color = @image.get(x, y)
+
+    # Find the bottom line
+    bx, by, new_color = find_change([ x, y ], 0, 1, @resolution / 2)
+    return unless bx
+    by -= 1
+
+    # Find the right edge
+    right_coords = ([ x ] * (by - y + 1)).zip(y..by)
+    rx, ry = find_change(right_coords, 1, 0, @resolution * 3)
+    unless rx
+      rx, ry = find_change([ bx, by + 1 ], 1, 0, @resolution * 3)
+      rx ||= x + @resolution * 3
+    end
+    rx -= 1
+
+    # Find the left edge
+    left_coords = ([ x ] * (by - y + 2)).zip(y..(by + 1))
+    lx, ly = find_change(left_coords, -1, 0, @resolution * 3)
+    lx ||= x - @resolution * 3
+    lx += 1
+
+    # Find the top edge
+    top_coords = (lx..rx).zip([ y ] * (rx - lx + 1))
+    tx, ty = find_change(top_coords, 0, -1, @resolution / 2)
+    ty ||= y - @resolution / 2
+    ty += 1
+
+    return [ lx, ty, rx, by ]
+  end
+
+  def find_change(coords, dx, dy, max_steps)
+    coords = [ coords ] unless coords[0].is_a?(Enumerable)
+
+    coords.each do |coord|
+      coord.push(@image.get(*coord)) unless coord.count == 3
+    end
+
+    1.upto(max_steps) do |i|
+      coords.each do |x, y, color|
+        tx, ty = x + dx * i, y + dy * i
+        begin
+          new_color = @image.get(tx, ty)
+          return tx, ty, new_color unless color == new_color
+        rescue RuntimeError
+          return nil
+        end
+      end
+    end
+    return nil
+  end
+
+  def load_image(page)
+    @page = page
+    return if page < 1 or page > @parser.pages
+
+    @tkc_image.destroy if @tkc_image
+    @image = TkPhotoImage.new(file: @parser.page_image(page, @resolution))
+    @tkc_image = TkcImage.new(
+      @canvas, 0, 0, image: @image, anchor: 'nw',
+      tags: 'image'
+    )
+
+    update_buttons
+  end
+
+  def update_buttons
+    @prev_page_button.configure(state: @page == 1 ? 'disabled' : 'normal')
+    @next_page_button.configure(
+      state: @page == @parser.pages ? 'disabled' : 'normal'
+    )
+  end
+end
+
 
 
 parser = PdfFileParser.new('f1040x--2016.pdf')
@@ -81,32 +231,8 @@ begin
 
   puts "File has #{parser.pages} pages"
 
-  root = TkRoot.new {
-    title("Mark Form Fields")
-  }
+  ui = MarkingUI.new(parser)
 
-  file_image = TkPhotoImage.new(file: parser.page_image(1, 144))
-
-  puts "Dims are #{file_image.width} x #{file_image.height}"
-
-  canvas = TkCanvas.new(root) do
-    width file_image.width
-    height 500
-    scrollregion "0 0 #{file_image.width} #{file_image.height}"
-  end
-  canvas.pack(fill: 'both', expand: 1, side: 'left')
-  canvas.bind('1') do |e|
-    puts "Got #{canvas.canvasx(e.x)}, #{canvas.canvasy(e.y)}"
-  end
-
-  TkcImage.new(canvas, 0, 0, image: file_image, anchor: 'nw')
-  vscroll = TkScrollbar.new(root) { orient 'vertical' }
-  canvas.yscrollbar(vscroll)
-  vscroll.pack(side: 'left', fill: 'y', expand: 1)
-
-  root.bind_all("MouseWheel") do |e|
-    canvas.yview_scroll(-e.delta, "units")
-  end
   Tk.mainloop
 
 ensure
